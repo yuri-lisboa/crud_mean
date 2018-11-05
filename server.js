67776c99d73eff16a58c9bdf938d5eac9a5e8d2c
yuri-lisboa <44594509+yuri-lisboa@users.noreply.github.com>
2018-11-05 14:01:24 -0200

    Completed CRUD

--- server.js
+++ server.js
@@ -1,12 +1,91 @@
 const express = require('express');
-const app = express ();
+const app = express();
+const bodyParser = require('body-parser');
 
-app.listen(3000, function(){
-    console.log('server running on port 3000')
+const ObjectId = require('mongodb').ObjectID
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://yuril:yuril123@ds263670.mlab.com:63670/testedb";
+
+app.use(bodyParser.urlencoded({extend: true}));
+
+MongoClient.connect(uri, (err, client )=> {
+    if(err) return  console.log(err);
+    db = client.db('testedb'); //DB name    
+
+   app.listen(3000, function () {
+    console.log('server  running  on port 3000');
+    }) 
+
 })
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    var cursor = db.collection('data').find()
+})
+
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
+
+    })
+})
+
+//Edita dados no
+app.route('/edit/:id')
+.get((req, res) => {
+  var id = req.params.id
+
+  db.collection('data').find(ObjectId(id)).toArray((err, result) => {
+    if (err) return res.send(err)
+    res.render('edit.ejs', { data: result })
+  })
+})
+.post((req, res) => {
+  var id = req.params.id
+  var name = req.body.name
+  var surname = req.body.surname
+
+  db.collection('data').updateOne({_id: ObjectId(id)}, {
+    $set: {
+      name: name,
+      surname: surname
+    }
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.redirect('/show')
+    console.log('Atualizado no Banco de Dados')
+  })
+})
+
+
+// Grva dados no banco
+app.post('/show', (req, res) => {
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+    })
+})
+
+app.route('/delete/:id')
+.get((req, res) => {
+  var id = req.params.id
+
+  db.collection('data').deleteOne({_id: ObjectId(id)}, (err, result) => {
+    if (err) return res.send(500, err)
+    console.log('Deletado do Banco de Dados!')
+    res.redirect('/show')
+  })
+})
